0e72719e48305ce9093dd846e57ae3011265be75
Darren Doherty <darren.doherty@gmail.com>
2018-11-22 21:00:05 +0000

    updated config to move environment settings out of git repo

--- server/config/config.js
+++ server/config/config.js
@@ -1,16 +1,15 @@
-var env = process.env.NODE_ENV || 'development';
+var env = process.env.NODE_ENV || "development";
 
 switch (env) {
-    case 'development':
-        process.env.PORT = 3000;
-        process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-        break;
+  case "development":
+  case "test":
+    var config = require("./config.json");
+    var envConfig = config[env];
+    Object.keys(envConfig).forEach(key => {
+      process.env[key] = envConfig[key];
+    });
+    break;
 
-    case 'test':
-        process.env.PORT = 3000;
-        process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-        break;
-
-    case 'production':
-        break;
+  case "production":
+    break;
 }
